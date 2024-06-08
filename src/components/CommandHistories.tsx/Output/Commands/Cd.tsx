8e9a7507fc3d1a5706122fce88ef8e40acce9d10
yuuki008 <s22ah261@gmail.com>
2024-06-08 23:15:52 +0900

    CD

---
+++ src/components/CommandHistories.tsx/Output/Commands/Cd.tsx
@@ -0,0 +1,66 @@
+import { CommandHistory, TerminalContext } from "@/context/TerminalContext";
+import { TerminalDirectory } from "@/utils/FileSystem";
+import { useContext, useEffect, useState } from "react";
+
+type Props = {
+  history: CommandHistory;
+};
+
+export const Cd = (props: Props) => {
+  const { currentDirectory, finishCommand, updateCurrentDirectory } =
+    useContext(TerminalContext);
+
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const root = (dir: TerminalDirectory) => {
+    let current = dir;
+    while (current.parent) {
+      current = current.parent;
+    }
+    return current;
+  };
+
+  // NOTE: 初回マウント時にのみ実行するため、eslintの警告を無効化
+  /* eslint-disable react-hooks/exhaustive-deps */
+  useEffect(() => {
+    const path = props.history.command.split(" ")[1];
+
+    if (path == "/" || path == "~" || !path) {
+      const dir = root(currentDirectory);
+      updateCurrentDirectory(dir);
+      return;
+    }
+
+    const tokens = path.split("/").filter((t) => t);
+    let current = currentDirectory;
+
+    for (const token of tokens) {
+      if (token == ".") continue;
+      if (token === "..") {
+        current = current.parent || current;
+        continue;
+      }
+
+      const child = current.children.find(
+        (c) => c.name === token && c instanceof TerminalDirectory
+      );
+
+      if (!child || !(child instanceof TerminalDirectory)) {
+        setErrorMessage(`cd: no such directory ${token}`);
+        break;
+      }
+
+      current = child;
+    }
+    updateCurrentDirectory(current);
+
+    finishCommand(props.history.id);
+  }, []);
+  /* eslint-enable react-hooks/exhaustive-deps */
+
+  if (errorMessage) {
+    return errorMessage;
+  }
+
+  return "";
+};
--- src/components/CommandHistories.tsx/Output/index.tsx
+++ src/components/CommandHistories.tsx/Output/index.tsx
@@ -1,25 +1,60 @@
+import React, { memo } from "react";
+import { Ls } from "./Commands/Ls";
+import { Cd } from "./Commands/Cd";
+import { Cat } from "./Commands/Cat";
+import { Help } from "./Commands/Help";
+import { History } from "./Commands/History";
+import { DateCommand as Date } from "./Commands/Date";
+import { Tree } from "./Commands/Tree";
+import { NotFoundCommand } from "./Commands/NotFoundCommand";
+import { ImgCat } from "./Commands/ImgCat";
+import { Clear } from "./Commands/Clear";
+import { Rm } from "./Commands/Rm";
+import { Wget } from "./Commands/Wget";
+import { Exit } from "./Commands/Exit";
 import { CommandHistory } from "@/context/TerminalContext";
-import { memo } from "react";
 
 type Props = {
   commandHistory: CommandHistory;
 };
 
-export const Output = memo((props: Props) => {
-  const { command, isRunning } = props.commandHistory;
+export const CommandOutput = memo((props: Props) => {
+  const { command } = props.commandHistory;
 
-  return (
-    <div>
-      <p className="text-white">
-        {isRunning ? (
-          <span className="text-green-500">Running...</span>
-        ) : (
-          <span>{command}</span>
-        )}
-      </p>
-    </div>
-  );
+  const tokens = command.trim().split(" ");
+  const commandName = tokens[0];
+
+  switch (commandName) {
+    case "ls":
+      return <Ls history={props.commandHistory} />;
+    case "cd":
+      return <Cd history={props.commandHistory} />;
+    // case "cat":
+    //   return <Cat history={props.commandHistory} />;
+    // case "imgcat":
+    //   return <ImgCat history={props.commandHistory} />;
+    // case "exit":
+    //   return <Exit history={props.commandHistory} />;
+    // case "help":
+    //   return <Help history={props.commandHistory} />;
+    // case "rm":
+    //   return <Rm history={props.commandHistory} />;
+    // case "history":
+    //   return <History history={props.commandHistory} />;
+    // case "date":
+    //   return <Date history={props.commandHistory} />;
+    // case "tree":
+    //   return <Tree history={props.commandHistory} />;
+    // case "clear":
+    //   return <Clear history={props.commandHistory} />;
+    // case "wget":
+    //   return <Wget history={props.commandHistory} />;
+    case "":
+      break;
+    default:
+      return <NotFoundCommand commandName={commandName} />;
+  }
 });
 
-Output.displayName = "Output";
-export default Output;
+CommandOutput.displayName = "CommandOutput";
+export default CommandOutput;
