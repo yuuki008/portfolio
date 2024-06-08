4c162a06e3c4eb4686e236ba1d998b7275f3a944
yuuki008 <s22ah261@gmail.com>
2024-06-08 22:06:27 +0900

    _app

---
+++ src/context/TerminalContext.tsx
@@ -0,0 +1,147 @@
+import { TerminalDirectory, TerminalFile } from "@/utils/FileSystem";
+import React, { createContext, useEffect, useState } from "react";
+import { Resource } from "@/pages/_app";
+
+type TerminalContextType = {
+  user: User | null;
+  commandHistories: string[];
+  isCommandRunning: boolean;
+  finishedFirstview: boolean;
+  currentDirectory: TerminalDirectory;
+  finishFirstview: () => void;
+  updateCurrentDirectory: (directory: TerminalDirectory) => void;
+  finishCommand: () => void;
+  username: (name: string) => Promise<boolean>;
+  login: (name: string, password: string) => Promise<boolean>;
+  executeCommand: (command: string) => void;
+  clearCommandHistories: () => void;
+};
+export const TerminalContext = createContext<TerminalContextType>(
+  {} as TerminalContextType
+);
+
+type Props = {
+  children: React.ReactNode;
+  rootDirectory: Resource;
+};
+
+type User = {
+  name: string;
+  role: string;
+};
+
+export function TerminalProvider({ children, rootDirectory }: Props) {
+  const [isCommandRunning, setIsCommandRunning] = useState(false);
+  const [commandHistories, setCommandHistories] = useState<string[]>([]);
+  const [user, setUser] = useState<User | null>(null);
+  const [finishedFirstview, setFinishedFirstview] = useState(false);
+
+  const [currentDirectory, setCurrentDirectory] =
+    useState<TerminalDirectory | null>(null);
+
+  const executeCommand = (command: string) => {
+    setIsCommandRunning(true);
+    setCommandHistories((prev) => [...prev, command]);
+  };
+
+  const finishCommand = () => setIsCommandRunning(false);
+
+  const login = async (name: string, password: string): Promise<boolean> => {
+    const res = await fetch("/api/login", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ name, password }),
+    }).then((res) => res.json());
+
+    return await new Promise((resolve) => {
+      setTimeout(() => {
+        const user = res.user;
+        if (user) {
+          resolve(true);
+          setUser(user);
+          setCommandHistories(["welcome"]);
+        } else {
+          resolve(false);
+        }
+      }, 1500);
+    });
+  };
+
+  const username = async (name: string): Promise<boolean> => {
+    const res = await fetch("/api/username", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ name }),
+    });
+
+    return await new Promise((resolve) => {
+      setTimeout(() => {
+        // NOTE: Visitor￼￼ユーザーはアクセスさせない
+        // if (res.status === 404) {
+        //   setUser({ name: "vistor", role: "vistor" });
+        //   setCommandHistories(["welcome"]);
+        // }
+        return resolve(res.status === 200);
+      }, 1500);
+    });
+  };
+
+  const updateCurrentDirectory = (directory: TerminalDirectory) =>
+    setCurrentDirectory(directory);
+
+  const finishFirstview = () => setFinishedFirstview(true);
+
+  const clearCommandHistories = () => setCommandHistories([]);
+
+  useEffect(() => {
+    if (!rootDirectory) return;
+
+    const root = new TerminalDirectory(rootDirectory.name);
+
+    const buildTree = (directory: TerminalDirectory, resource: Resource) => {
+      resource.children.forEach((child) => {
+        if (child.type === "file") {
+          const childFile = new TerminalFile(child.name);
+          childFile.parent = directory;
+          directory.children.push(childFile);
+          return;
+        }
+
+        const childDirectory = new TerminalDirectory(child.name);
+        directory.children.push(childDirectory);
+        childDirectory.parent = directory;
+        buildTree(childDirectory, child);
+      });
+    };
+
+    buildTree(root, rootDirectory);
+    setCurrentDirectory(root);
+  }, [rootDirectory]);
+
+  if (!currentDirectory) return <></>;
+
+  return (
+    <TerminalContext.Provider
+      value={{
+        user,
+        commandHistories,
+        isCommandRunning,
+        finishedFirstview,
+        currentDirectory,
+        finishFirstview,
+        updateCurrentDirectory,
+        finishCommand,
+        username,
+        login,
+        executeCommand,
+        clearCommandHistories,
+      }}
+    >
+      {children}
+    </TerminalContext.Provider>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,88 @@
+import { TerminalProvider } from "@/context/TerminalContext";
 import "@/styles/globals.css";
+import "github-markdown-css/github-markdown-light.css";
 import type { AppProps } from "next/app";
+import { Space_Mono } from "next/font/google";
+import { statSync, readdirSync } from "fs";
+import { join, basename } from "path";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const mono = Space_Mono({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export type DirectoryResource = {
+  name: string;
+  type: "directory";
+  children: Resource[];
+};
+
+export type FileResource = {
+  name: string;
+  type: "file";
+};
+
+export type Resource = DirectoryResource | FileResource;
+
+type Props = {
+  rootDirectory: Resource;
+} & AppProps;
+
+function App({ Component, pageProps, rootDirectory }: Props) {
+  return (
+    <TerminalProvider rootDirectory={rootDirectory}>
+      <main className={`${mono.className} bg-black`}>
+        <div className="overflow-y-scroll terminal min-h-screen bg-black">
+          <Component {...pageProps} />
+        </div>
+      </main>
+    </TerminalProvider>
+  );
 }
+
+const readDirectory = (directoryPath: string): Resource => {
+  const stat = statSync(directoryPath);
+  if (!stat.isDirectory()) {
+    throw new Error("The provided path is not a directory.");
+  }
+
+  const files = readdirSync(directoryPath);
+  const directory: Resource = {
+    name: basename(directoryPath),
+    type: "directory",
+    children: [],
+  };
+
+  files.forEach((file) => {
+    const filePath = join(directoryPath, file);
+    const fileStat = statSync(filePath);
+    if (fileStat.isDirectory()) {
+      directory.children.push(readDirectory(filePath));
+    } else {
+      directory.children.push({
+        name: file,
+        type: "file",
+      });
+    }
+  });
+
+  return directory;
+};
+
+App.getInitialProps = async () => {
+  const root = join(process.cwd(), "public");
+  let rootDirectory: Resource;
+
+  try {
+    rootDirectory = readDirectory(root);
+  } catch (error) {
+    console.error("Error reading directory:", error);
+    rootDirectory = { name: "terminal", type: "directory", children: [] };
+  }
+
+  return {
+    rootDirectory,
+  };
+};
+
+export default App;
