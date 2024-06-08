323539cbee0afe3d4523ade776a2f0862d32d549
yuuki008 <s22ah261@gmail.com>
2024-06-08 23:16:04 +0900

    LS

---
+++ src/components/CommandHistories.tsx/Output/Commands/Ls.tsx
@@ -0,0 +1,33 @@
+import { CommandHistory, TerminalContext } from "@/context/TerminalContext";
+import { classes } from "@/utils/classes";
+import { TerminalDirectory } from "@/utils/FileSystem";
+import { useContext, useEffect, useState } from "react";
+
+type Props = {
+  history: CommandHistory;
+};
+
+export const Ls = (props: Props) => {
+  const { currentDirectory, finishCommand } = useContext(TerminalContext);
+
+  const [directory, _setDirectory] = useState(currentDirectory);
+
+  /* eslint-disable react-hooks/exhaustive-deps */
+  useEffect(() => finishCommand(props.history.id), []);
+  /* eslint-enable react-hooks/exhaustive-deps */
+
+  return (
+    <div className="w-full">
+      {directory?.children.map((child: any, index: number) => (
+        <div
+          className={classes(
+            child instanceof TerminalDirectory ? "text-blue-500" : "text-white"
+          )}
+          key={index}
+        >
+          {child.name}
+        </div>
+      ))}
+    </div>
+  );
+};
