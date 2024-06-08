9e15904659271cc55946c7ac6fe87373f4a4b8a3
yuuki008 <s22ah261@gmail.com>
2024-06-08 22:00:23 +0900

    fs と path を読み込めるように

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+import { config } from "process";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.path = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
