dcc80ee70b5359216b4c538ad880f603a269098d
yuuki008 <s22ah261@gmail.com>
2024-06-11 17:02:50 +0900

    OpenGraph の設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,25 +4,25 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <title>{`yuuki008 - Portfolio `}</title>
+      <title>{`Yuuki008 - Portfolio `}</title>
       <meta
         data-react-helmet="true"
         name="description"
-        content="Experience a unique terminal-style portfolio website, designed to showcase projects and skills in a hacker-themed interface. Navigate through various commands and immerse yourself in a tech-savvy environment."
+        content="This is a terminal-style portfolio website. Enjoy your journey ..."
       ></meta>
 
       {/* Open Graph */}
       <meta
         data-react-helmet="true"
         property="og:title"
-        content="yuuki008 - Portfolio"
+        content="Yuuki008 - Portfolio"
       ></meta>
       <meta
         data-react-helmet="true"
         property="og:description"
-        content="Experience a unique terminal-style portfolio website, designed to showcase projects and skills in a hacker-themed interface. Navigate through various commands and immerse yourself in a tech-savvy environment."
+        content="This is a terminal-style portfolio website. Enjoy your journey ..."
       ></meta>
-      <meta property="og:image" content="/opengraph-image.png" />
+      <meta property="og:image" content="/images/opengraph-image.png" />
       <meta property="og:image:type" content="article" />
       <meta property="og:image:width" content="1068" />
       <meta property="og:image:height" content="609" />
@@ -41,14 +41,14 @@ export default function Document() {
       <meta
         data-react-helmet="true"
         property="twitter:title"
-        content="yuuki008 - Portfolio"
+        content="Yuuki008 - Portfolio"
       ></meta>
       <meta
         data-react-helmet="true"
         property="twitter:description"
         content="Experience a unique terminal-style portfolio website, designed to showcase projects and skills in a hacker-themed interface. Navigate through various commands and immerse yourself in a tech-savvy environment."
       ></meta>
-      <meta name="twitter:image" content="/opengraph-image.png" />
+      <meta name="twitter:image" content="/images/opengraph-image.png" />
       <meta name="twitter:image:type" content="article" />
       <meta name="twitter:image:width" content="1068" />
       <meta name="twitter:image:height" content="609" />
